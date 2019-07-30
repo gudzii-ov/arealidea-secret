855cb3b15786a642bbd3722df724589cabde2f69
Oleg Gudzii <oleg198679@gmail.com>
2019-07-30 11:31:19 +0300

    Add dummy routes

--- server/index.js
+++ server/index.js
@@ -3,8 +3,22 @@ import express from 'express';
 const server = express();
 const port = 3000;
 
-server.get('/', (req, res) => {
-  res.send('Welcome to Secret service');
-});
+server.route('/')
+  .get((req, res) => {
+    res.send('Welcome to Secret service!');
+  })
+  .post((req, res) => {
+    res.send('Dummy post secret');
+  });
 
-server.listen(port, () => console.log(`Secret server is running on port ${port}`));
+server.route('/:secretId')
+  .get((req, res) => {
+    const { secretId } = req.params;
+    res.send(`There should be secrete receiving page.\n Secret id: ${secretId}`);
+  })
+  .delete((req, res) => {
+    const { secretId } = req.params;
+    res.send(`There should be secrete removing page.\n Secret id: ${secretId}`);
+  });
+
+server.listen(port, () => console.log(`Secret server is running on port ${port}.`));
