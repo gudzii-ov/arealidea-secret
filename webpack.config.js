74493dc142b1cc1b12e36969f618b00ac239b16a
Oleg Gudzii <oleg198679@gmail.com>
2019-07-30 14:24:16 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: [
+    `${__dirname}/front/index.jsx`,
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist/public`,
+    publicPath: '/assets/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
